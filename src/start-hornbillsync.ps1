7d4457c97f6cdcbf1caa87a0c81271be1fbbded0
Ed Haynes <23118000+WebedMJ@users.noreply.github.com>
2019-04-15 17:04:15 +0100

    Adding json construction

--- src/start-hornbillsync.ps1
+++ src/start-hornbillsync.ps1
@@ -2,14 +2,20 @@
 # e.g. on ACI see https://docs.microsoft.com/en-us/azure/container-instances/container-instances-environment-variables
 $HBDirectory = 'c:\Hornill_Import'
 $HBConfig = Get-Content -Path C:\Hornbill_Import\conftemplate.json | ConvertFrom-Json
+$HBConfig.APIKey = $env:HBAPIKey
+$HBConfig.InstanceId = $env:HBInstanceId
 $HBConfig.AzureConf.Tenant = $env:AzureADTenant
-#
-# Add the other required secrets to conf from env...
-#
+$HBConfig.AzureConf.ClientID = $env:AzureADClientID
+$HBConfig.AzureConf.ClientSecret = $env:AzureADClientSecret
+($HBConfig.AzureConf.UsersByGroupID[0]).ObjectID = $env:AzureADGroupID1
+($HBConfig.AzureConf.UsersByGroupID[0]).Name = $env:AzureADGroupName1
+($HBConfig.AzureConf.UsersByGroupID[1]).ObjectID = $env:AzureADGroupID2
+($HBConfig.AzureConf.UsersByGroupID[1]).Name = $env:AzureADGroupName2
 $HBConfig | ConvertTo-Json -Depth 4 | Out-File $(Join-Path -Path $HBDirectory -ChildPath 'conf.json')
 $params = @{
     FilePath         = '{0}\Azure2UserImport_amd64.exe' -f $HBDirectory
     WorkingDirectory = $HBDirectory
     ArgumentList     = '-dryrun=true'
+    Wait             = $true
 }
 Start-Process @params
