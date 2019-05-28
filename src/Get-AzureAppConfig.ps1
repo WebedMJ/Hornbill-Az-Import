d6a81a13f6d756235e6ab9584f87fcf7f9cff73a
Ed Haynes <23118000+WebedMJ@users.noreply.github.com>
2019-05-28 17:13:50 +0100

    (╯°□°）╯︵ ┻━┻

--- src/Get-AzureAppConfig.ps1
+++ src/Get-AzureAppConfig.ps1
@@ -1,5 +1,6 @@
 <#
-NOT WORKING YET (╯°□°）╯︵ ┻━┻
+ONLY WORKS IN Windows PS NOT Core/6.x (╯°□°）╯︵ ┻━┻
+6.x Invoke-RestMethod doesn't like commas in the authorization header...
 
 Host: example.azconfig.io
 Date: Fri, 11 May 2018 18:48:36 GMT
@@ -16,29 +17,36 @@ string-To-Sign=
 https://github.com/Azure/AppConfiguration/blob/master/docs/REST/authentication.md
 /#>
 
-[string]$appconf = 'https://example.azconfig.io;Id=xxxxxxxxxxxxxx;Secret=xxxxxxxxxxxxxxxxxxxxxxxx'
-[uri]$hosturi = $appconf.Split(';')[0]
-[string]$credential = (($appconf.Split(';')[1]).Substring(3)).Split(':')[1]
-# [string]$credential = (($appconf.Split(';')[1]).Substring(3))
-$configuri = [System.UriBuilder]::new($hosturi.Scheme, $hosturi.Host, "443", 'keys', '')
+# [string]$appconf = 'https://example.azconfig.io;Id=xxxxxxxxxxxxxx;Secret=xxxxxxxxxxxxxxxxxxxxxxxx'
+# [uri]$hosturi = $appconf.Split(';')[0]
+# [string]$credential = (($appconf.Split(';')[1]).Substring(3)).Split(':')[1]
+# # [string]$credential = (($appconf.Split(';')[1]).Substring(3))
+# $configuri = [System.UriBuilder]::new($hosturi.Scheme, $hosturi.Host, "443", 'keys', '')
+[uri]$configuri = 'https://example.azconfig.io'
+$configid = 'xxxx'
+$configsecret = 'xxxx'
+
+$uri = [System.UriBuilder]::new($configuri.Scheme, $configuri.Host, "443", 'kv', '')
 $body = ''
 $GMTTime = (Get-Date).ToUniversalTime().toString('R')
 $sha256 = New-Object System.Security.Cryptography.SHA256CryptoServiceProvider
 $bodyhash = [Convert]::ToBase64String($sha256.ComputeHash([Text.Encoding]::ASCII.GetBytes($body)))
-$signedheaders = 'x-ms-date;Host;x-ms-content-sha256'
-$signedheadersvalue = '{0};{1};{2}' -f $GMTTime, $hosturi.Host, $bodyhash
-$b64secret = ($appconf.Split(';')[2]).Substring(7)
+$signedheaders = 'Host;x-ms-date;x-ms-content-sha256'
+$signedheadersvalue = '{0};{1};{2}' -f $configuri.Host, $GMTTime, $bodyhash
+# $b64secret = ($appconf.Split(';')[2]).Substring(7)
 $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
-$hmacsha.key = [Convert]::FromBase64String($b64secret)
-$StringToSign = "GET`n{0}`n{1}" -f $configuri.Path, $signedheadersvalue
+$hmacsha.key = [Convert]::FromBase64String($configsecret)
+$urimethod = 'GET'
+$StringToSign = "{0}`n{1}`n{2}" -f $urimethod, $uri.Uri.PathAndQuery, $signedheadersvalue
 $signature = [Convert]::ToBase64String($hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($StringToSign)))
-$Authorization = 'HMAC-SHA256 Credential={0};SignedHeaders={1};Signature={2}' -f $credential, $signedheaders, $signature
+# $Authorization = 'HMAC-SHA256 Credential={0}, SignedHeaders={1}, Signature={2}' -f $configid, $signedheaders, $signature
+$Authorization = "HMAC-SHA256 Credential={0}, SignedHeaders={1}, Signature={2}" -f $configid, $signedheaders, $signature
 $headers = @{
-    Host                  = $hosturi.Host
+    Host                  = $configuri.Host
     'x-ms-date'           = $GMTTime
     'x-ms-content-sha256' = $bodyhash
     Authorization         = $Authorization
 }
-Invoke-WebRequest -Uri $configuri.Uri -Headers $headers -Method GET
+$r = Invoke-RestMethod -Uri $uri.Uri -Headers $headers -Method $urimethod
 
 $error[0].Exception.Response.Headers
