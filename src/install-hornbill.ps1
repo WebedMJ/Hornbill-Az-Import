0b6b61058bf611812553c1bf8d97dc7e9cdc3edb
Ed Haynes <23118000+WebedMJ@users.noreply.github.com>
2019-05-28 12:29:47 +0100

    Updated to use v2.0.2

--- src/install-hornbill.ps1
+++ src/install-hornbill.ps1
@@ -1,16 +1,17 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
-$HB_SHA256 = "DBBDF4AF4ECC5DF1CADBDB8190160804DDB22830B1E32D215D6D03242AF65BA2"
-$HB_VERSION = "1.4.2"
+$HBSHA256 = "CF98B8F47BE325524FD716520FA0CD3D765D453265BF5180330AF4ABBD92892F"
+$HBVersion = "2.0.2"
 $HBDirectory = 'c:\Hornbill_Import'
+$HBZip = '{0}\Azure2UserImport.zip' -f $env:TEMP
 $params = @{
     Method  = 'Get'
-    Uri     = 'https://github.com/hornbill/goAzure2HUserImport/releases/download/v{0}/Azure2UserImport_{1}.zip' -f $HB_VERSION, $HB_VERSION
-    OutFile = 'c:\Azure2UserImport.zip'
+    Uri     = 'https://github.com/hornbill/goAzure2HUserImport/releases/download/v{0}/Azure2UserImport_{1}.zip' -f $HBVersion, $HBVersion
+    OutFile = $HBZip
 }
 Invoke-WebRequest @params
-if ((Get-FileHash c:\Azure2UserImport.zip -Algorithm sha256).Hash -ne $HB_SHA256) { exit 1 }
-Expand-Archive -Path c:\Azure2UserImport.zip -DestinationPath $HBDirectory
-Remove-Item c:\Azure2UserImport.zip -Force
+if ((Get-FileHash $HBZip -Algorithm sha256).Hash -ne $HBSHA256) { exit 1 }
+Expand-Archive -Path $HBZip -DestinationPath $HBDirectory
+Remove-Item $HBZip -Force
 Remove-Item $(Join-Path -Path $HBDirectory -ChildPath 'conf.json')
