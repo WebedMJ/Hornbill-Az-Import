db5a20c2ac3d6fcd615b1443a7d0b076a6a9f82b
Ed Haynes <23118000+WebedMJ@users.noreply.github.com>
2019-05-29 17:53:51 +0100

    WIP

--- src/Get-AzureAppConfig.ps1
+++ src/Get-AzureAppConfig.ps1
@@ -1,52 +1,44 @@
 <#
-ONLY WORKS IN Windows PS NOT Core/6.x (╯°□°）╯︵ ┻━┻
+ONLY WORKS IN Windows PS 5.x NOT Core/6.x (╯°□°）╯︵ ┻━┻
 6.x Invoke-RestMethod doesn't like commas in the authorization header...
-
-Host: example.azconfig.io
-Date: Fri, 11 May 2018 18:48:36 GMT
-x-ms-content-sha256: 47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=
-Authorization: HMAC-SHA256 Credential=a4016f0fa8fb0ef2, SignedHeaders=Host;x-ms-date;x-ms-content-sha256, Signature=jMXmttaxBJ0NmLlFKLZUkI8jdFu/8yqcTYzbkI3DGdU=
-
-x-ms-content-sha256	base64 encoded SHA256 hash of the request body. It must be provided even of there is no body. base64_encode(SHA256(body))
-
-string-To-Sign=
-            "GET" + '\n' +                                                                                      // VERB
-            "/kv?fields=*" + '\n' +                                                                             // path_and_query
-            "Fri, 11 May 2018 18:48:36 GMT;example.azconfig.io;47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="    // signed_headers_values
-
-https://github.com/Azure/AppConfiguration/blob/master/docs/REST/authentication.md
-/#>
-
-# [string]$appconf = 'https://example.azconfig.io;Id=xxxxxxxxxxxxxx;Secret=xxxxxxxxxxxxxxxxxxxxxxxx'
-# [uri]$hosturi = $appconf.Split(';')[0]
-# [string]$credential = (($appconf.Split(';')[1]).Substring(3)).Split(':')[1]
-# # [string]$credential = (($appconf.Split(';')[1]).Substring(3))
-# $configuri = [System.UriBuilder]::new($hosturi.Scheme, $hosturi.Host, "443", 'keys', '')
-[uri]$configuri = 'https://example.azconfig.io'
-$configid = 'xxxx'
-$configsecret = 'xxxx'
-
-$uri = [System.UriBuilder]::new($configuri.Scheme, $configuri.Host, "443", 'kv', '?$select=key,value')
-$body = ''
-$GMTTime = (Get-Date).ToUniversalTime().toString('R')
-$sha256 = New-Object System.Security.Cryptography.SHA256CryptoServiceProvider
-$bodyhash = [Convert]::ToBase64String($sha256.ComputeHash([Text.Encoding]::ASCII.GetBytes($body)))
-$signedheaders = 'Host;x-ms-date;x-ms-content-sha256'
-$signedheadersvalue = '{0};{1};{2}' -f $configuri.Host, $GMTTime, $bodyhash
-# $b64secret = ($appconf.Split(';')[2]).Substring(7)
-$hmacsha = New-Object System.Security.Cryptography.HMACSHA256
-$hmacsha.key = [Convert]::FromBase64String($configsecret)
-$urimethod = 'GET'
-$StringToSign = "{0}`n{1}`n{2}" -f $urimethod, $uri.Uri.PathAndQuery, $signedheadersvalue
-$signature = [Convert]::ToBase64String($hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($StringToSign)))
-# $Authorization = 'HMAC-SHA256 Credential={0}, SignedHeaders={1}, Signature={2}' -f $configid, $signedheaders, $signature
-$Authorization = "HMAC-SHA256 Credential={0}, SignedHeaders={1}, Signature={2}" -f $configid, $signedheaders, $signature
-$headers = @{
-    Host                  = $configuri.Host
-    'x-ms-date'           = $GMTTime
-    'x-ms-content-sha256' = $bodyhash
-    Authorization         = $Authorization
+#>
+function Get-AppConfigKeyValues {
+    param (
+        [Parameter(Mandatory = $true)]
+        [uri]$uri,
+        [Parameter(Mandatory = $true)]
+        [string]$id,
+        [Parameter(Mandatory = $true)]
+        [string]$secret,
+        [Parameter(Mandatory = $false)]
+        [string]$keyname
+    )
+    if ($keyname) {
+        $keyquery = 'kv/{0}' -f $keyname
+    } else {
+        $keyquery = 'kv'
+    }
+    $appcfguri = [System.UriBuilder]::new($uri.Scheme, $uri.Host, "443", $keyquery, '?$select=key,value')
+    $body = ''
+    $GMTTime = (Get-Date).ToUniversalTime().toString('R')
+    $sha256 = New-Object System.Security.Cryptography.SHA256CryptoServiceProvider
+    $bodyhash = [Convert]::ToBase64String($sha256.ComputeHash([Text.Encoding]::ASCII.GetBytes($body)))
+    $signedheaders = 'Host;x-ms-date;x-ms-content-sha256'
+    $signedheadersvalue = '{0};{1};{2}' -f $uri.Host, $GMTTime, $bodyhash
+    $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+    $hmacsha.key = [Convert]::FromBase64String($secret)
+    $urimethod = 'GET'
+    $StringToSign = "{0}`n{1}`n{2}" -f $urimethod, $appcfguri.Uri.PathAndQuery, $signedheadersvalue
+    $signature = [Convert]::ToBase64String($hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($StringToSign)))
+    $Authorization = "HMAC-SHA256 Credential={0}, SignedHeaders={1}, Signature={2}" -f $id, $signedheaders, $signature
+    $headers = @{
+        Host                  = $uri.Host
+        'x-ms-date'           = $GMTTime
+        'x-ms-content-sha256' = $bodyhash
+        Authorization         = $Authorization
+    }
+    $r = Invoke-RestMethod -Uri $appcfguri.Uri -Headers $headers -Method $urimethod
+    return $r
 }
-$r = Invoke-RestMethod -Uri $uri.Uri -Headers $headers -Method $urimethod
 
-$error[0].Exception.Response.Headers
+# Get-AppConfigKeyValues -uri $configuri -id $configid -secret $configsecret -keyname $configkey
--- src/start-hornbillsync.ps1
+++ src/start-hornbillsync.ps1
@@ -1,18 +1,34 @@
-# WIP - Currently garbles API keys...
-# TO-DO - Move to using Azure App Configuration
+# Must run in PS 6 as it garbles API keys in PS 5...
+# WIP - Move to using Azure App Configuration
 # Pass required json attributes as environment variables at container creation / run time
 # e.g. on ACI see https://docs.microsoft.com/en-us/azure/container-instances/container-instances-environment-variables
 $HBDirectory = 'c:\Hornbill_Import'
+$appconfig = .\Get-AzureAppConfig.ps1
 $HBConfig = Get-Content -Path $HBDirectory\conftemplate.json | ConvertFrom-Json
-$HBConfig.APIKey = $env:HBAPIKey
-$HBConfig.InstanceId = $env:HBInstanceId
-$HBConfig.AzureConf.Tenant = $env:AzureADTenant
-$HBConfig.AzureConf.ClientID = $env:AzureADClientID
-$HBConfig.AzureConf.ClientSecret = $env:AzureADClientSecret
-($HBConfig.AzureConf.UsersByGroupID[0]).ObjectID = $env:AzureADGroupID1
-($HBConfig.AzureConf.UsersByGroupID[0]).Name = $env:AzureADGroupName1
-# ($HBConfig.AzureConf.UsersByGroupID[1]).ObjectID = $env:AzureADGroupID2
-# ($HBConfig.AzureConf.UsersByGroupID[1]).Name = $env:AzureADGroupName2
+[uri]$configuri = $env:cfguri # move to appconfig script to run in ps 5
+$configid = $env:cfgid # move to appconfig script to run in ps 5
+$configsecret = $env:cfgsecret # move to appconfig script to run in ps 5
+
+# TO DO
+# Need to modify script to get environment vars within PS 5 process to save passing them through here (uri, id, secret)
+$HBConfig.APIKey = (
+    powershell.exe -noprofile -Command {
+        . .\Get-AzureAppConfig.ps1;
+        (Get-AppConfigKeyValues -uri 'xx' -id 'xx' -secret 'xx' -keyname 'APIKey').value })
+# $HBConfig.InstanceId = (
+#     Get-AppConfigKeyValues -uri $configuri -id $configid -secret $configsecret -keyname 'InstanceId').value
+# $HBConfig.AzureConf.Tenant = (
+#     Get-AppConfigKeyValues -uri $configuri -id $configid -secret $configsecret -keyname 'AzureConf.Tenant').value
+# $HBConfig.AzureConf.ClientID = (
+#     Get-AppConfigKeyValues -uri $configuri -id $configid -secret $configsecret -keyname 'AzureConf.ClientID').value
+# $HBConfig.AzureConf.ClientSecret = (
+#     Get-AppConfigKeyValues -uri $configuri -id $configid -secret $configsecret -keyname 'AzureConf.ClientSecret').value
+# ($HBConfig.AzureConf.UsersByGroupID[0]).ObjectID = (
+#     Get-AppConfigKeyValues -uri $configuri -id $configid -secret $configsecret -keyname 'AzureConf.UsersByGroupID.0.ObjectID').value
+# ($HBConfig.AzureConf.UsersByGroupID[0]).Name = (
+#     Get-AppConfigKeyValues -uri $configuri -id $configid -secret $configsecret -keyname 'AzureConf.UsersByGroupID.0.Name').value
+
+# This line only works in PS 6... https://github.com/PowerShell/PowerShell/issues/2632
 $HBConfig | ConvertTo-Json -Depth 4 | Out-File $(Join-Path -Path $HBDirectory -ChildPath 'conf.json')
 $params = @{
     FilePath         = '{0}\Azure2UserImport_x64.exe' -f $HBDirectory
